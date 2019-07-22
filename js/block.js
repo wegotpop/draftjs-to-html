00b0a20d029b2206c25eb9685e7473c802151237
Amit Shah <amit@amwam.me>
2019-07-22 11:46:25 +0100

    Wrap images in a div

--- js/block.js
+++ js/block.js
@@ -337,7 +337,7 @@ function getEntityMarkup(
     if (!alignment || alignment === 'none') {
       alignment = 'center';
     }
-    return `<img src="${entity.data.src}" alt="${entity.data.alt}" style="text-align:${alignment};height: ${entity.data.height};width: ${entity.data.width}"/>`;
+    return `<div style="text-align:${alignment};"><img src="${entity.data.src}" alt="${entity.data.alt}" style="height: ${entity.data.height};width: ${entity.data.width}"/></div>`;
   }
   if (entity.type === 'EMBEDDED_LINK') {
     return `<iframe width="${entity.data.width}" height="${entity.data.height}" src="${entity.data.src}" frameBorder="0"></iframe>`;
